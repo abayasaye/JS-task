34fd5f53209d8e7b4b41ebdfd742616af1d3d47a
abayasaye <abay400@gmail.com>
2022-05-15 09:44:21 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,85 @@
+// פונקציית void 
+// // function printhello(){   הגדרת הפונקצייה
+
+// //     console.log("hello ")
+// // }
+// // printhello();הפעלת הפונקצייה
+
+
+
+// הגדרת הפונקצייה
+// function sumup(number1,number2){
+
+//     var result= number1+number2;הגדרת המשתנה
+=    //    return result;החזרת המשתנה
+
+//     console.log(result)הדפסת המשתנה
+    // console.log(sumup(5,9))   בלי שמירת משתנים הדפסת המשתנה
+
+// // }
+
+
+// var firstnumber=5; הגדרת המשתנה
+
+// // var sum =sumup(5,9)הפעלת הפונקצייה
+// console.log(sum)הדפסת המשתנה
+
+// var num1=+prompt("enter a number1") הגדרת המשתנה
+// var num2=+prompt("enter a number2") הגדרת המשתנה
+// sumup(num1,num2);
+
+// פונקצייה עם תנאי if 
+// function name (parms){
+//     if (condition){
+//         return 10;
+//     }
+//   return ("hello");
+// }
+
+
+//  פונקצייה מוסוות 
+
+// function sumfunction(){
+
+//     for (var i = 0 ; i< parseint(getFromPrompt ("enter a number")) ;i++){
+//         document.write (i)
+//         document.write (getTextForDom("first name"))
+//         document.write (getTextForDom("last name"))
+//         document.write (getTextForDom("city"))
+//     }
+     
+// }
+// function getTextForDom(title){
+
+//     return getfromprompt(title)+"<br>"
+// }
+// function getfromprompt(text){
+
+//     return prompt("please enter" + text );
+     
+// }
+// sumfunction()
+
+
+
+// function sumup(){
+// var firstName =prompt("enter name")
+// var number = +prompt("enter number")
+// for ( i=0; i < number;i++){
+//     document.writeln(i+"hello" + firstName + "<br>")
+// }
+// sumup()
+// }
+
+// function getfromprompt(text){
+
+//     varfirstName=prompt("please enter"+  text );
+// }
+// getfromprompt(" first Name")
+// getfromprompt(" last Name")
+
+// function getfromprompt(text){
+
+//     var result=prompt("please enter"+  text );
+//     return result;
+// }
